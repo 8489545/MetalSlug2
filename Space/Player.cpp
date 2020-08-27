e89d918b1bca617f7fbe6639ed30992f7b68dd49
8489545 <38340705+8489545@users.noreply.github.com>
2020-08-27 16:27:36 +0900

    플레이어 클래스 비움

--- Space/Player.cpp
+++ Space/Player.cpp
@@ -3,47 +3,18 @@
 
 Player::Player(Vec2 Pos)
 {
-	m_PlayerTop = new Animation();
-	m_PlayerTop->Init(0.1f, true, BigImage);
-	m_PlayerTop->AddContinueFrame(L"Painting/Player/Right/Top.bmp", 1, 11);
-
-	m_PlayerBottom = new Animation();
-	m_PlayerBottom->Init(0.1f, true, BigImage);
-	m_PlayerBottom->AddContinueFrame(L"Painting/Player/Right/Bottom.bmp", 1, 11);
-
-	m_PlayerTop->m_Position = Pos;
-
-	m_PlayerDirection = RIGHT;
-
-	m_State = State::IDLE;
-
 }
 
 Player::~Player()
 {
 }
 
-void Player::SetPlayerAni()
-{
-	if (m_PlayerDirection == LEFT)
-	{
-		Dire = L"Left";
-	}
-	else if (m_PlayerDirection == RIGHT)
-	{
-		Dire = L"Right";
-	}
-}
+
 void Player::Update(float deltaTime, float Time)
 {
-	SetPlayerAni();
-
-	m_PlayerTop->Update(deltaTime,Time);
-	m_PlayerBottom->Update(deltaTime, Time);
 }
 
 void Player::Render()
 {
-	m_PlayerTop->Render();
-	m_PlayerBottom->Render();
+
 }
--- Space/Player.h
+++ Space/Player.h
@@ -1,25 +1,10 @@
 #pragma once
-enum class State
-{
-	IDLE,
-	RUN,
-	NONE
-};
 class Player : public Object
 {
-	Animation* m_PlayerTop;
-	Animation* m_PlayerBottom;
+
 public:
 	Player(Vec2 Pos);
 	~Player();
-
-	std::wstring Dire;
-	State m_State;
-
-	int m_PlayerDirection;
-
-	bool m_isChangedPlayerState;
-	void SetPlayerAni();
 
 	void Update(float deltaTime,float Time);
 	void Render();
